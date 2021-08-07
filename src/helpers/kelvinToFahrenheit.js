113364a313b03931a64fc8e809175409774c7277
gitpeut <peut@peut.org>
2021-08-07 02:14:56 +0200

    af, met kleine wijzigingen

---
+++ src/helpers/kelvinToFahrenheit.js
@@ -0,0 +1,9 @@
+
+
+function kelvinToCelsius( kelvins ){
+    const celsius = Math.round( kelvins - 273.15 ); // from Kelvin to to Celsius
+
+    return( `${celsius} °C`);
+}
+
+export default kelvinToCelsius;
